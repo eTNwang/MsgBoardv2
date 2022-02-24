4832a933d638943ab78a90aafd9c629bd0356f27
eTNwang <erictnwang@gmail.com>
2022-02-24 12:47:42 -0500

    finalfixed

--- src/components/ReplyList.js
+++ src/components/ReplyList.js
@@ -1,77 +1,27 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState } from 'react'
-import s from 'styled-components'
-import Input from './Input'
-import ReplyList from './ReplyList'
+import Reply from './Reply'
 
-const Reply = ({ layer, reply, setReply }) => {
-  const [state, setState] = React.useState(
-    [],
-  )
+const FinalReplyList = ({ replyList }) => (
+  <div>
+    {replyList.map((reply, ind) => (
+      <Reply key={reply.id} reply={reply} />
+    ))}
+  </div>
+)
 
-  const [votes, setVotes] = useState(0)
+const ReplyList = ({ replies }) => {
+  // const addPost= post => {
+  //   const oldposts = state.posts
+  //   const newposts = oldposts.push(post)
+  //   setState({
+  //     posts: newposts,
+  //   })
+  // }
 
-  const upVote = () => {
-    setVotes(votes + 1)
-  }
+  const replyList = replies
 
-  const downVote = () => {
-    setVotes(votes - 1)
-  }
-  return (
-
-    <>
-      <Wrapper>
-        <p>
-          Responder:
-          {' '}
-          {reply.sender}
-        </p>
-        <p>
-          Reply Content:
-          {reply.content}
-        </p>
-
-        <VoteTracker>
-          <Flex>
-            <button type="button" onClick={upVote}>Up-vote</button>
-          </Flex>
-
-          <Flex>
-            <>{votes}</>
-          </Flex>
-
-          <Flex>
-            <button type="button" onClick={downVote}>Down-vote</button>
-          </Flex>
-        </VoteTracker>
-
-        <div>Why not send a reply ?</div>
-
-        {reply.layer < 2
-        && (
-        <>
-          <Input keyid={reply.id + 1} passlayer={reply.layer} posts={state} setPosts={setState} />
-          <ReplyList replies={state} />
-        </>
-        )}
-
-      </Wrapper>
-    </>
-  )
+  return <FinalReplyList replyList={replyList} />
 }
 
-export default Reply
-
-const Wrapper = s.div`
-  border: 10px solid blue;
-  padding: 5;
-`
-
-const VoteTracker = s.div`
-  display: flex;
-  flex-direction: row;
-
-`
-const Flex = s.div`
-  margin: 5px;
-`
+export default ReplyList
