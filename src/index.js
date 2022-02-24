c141618608f8d2d05090879a6be8f965e5699571
eTNwang <erictnwang@gmail.com>
2022-02-23 20:40:36 -0500

    netifly changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import ReactDOM from "react-dom"
+import React from 'react'
+
+import ReactDOM from 'react-dom'
 // import App from "./App"
 import App from './App'
 
-const app = document.getElementById("app")
+const app = document.getElementById('app')
 ReactDOM.render(<App />, app)
